8aed7723cbb0dd2ce8caad43f10dbefaba3a595d
Elwardi <fadeli.elwardi@tu-darmstadt.de>
2023-07-20 07:31:51 +0000

    Working Shape optimization on PitzDaily

--- pitzDaily/getPressDiff.sh
+++ pitzDaily/getPressDiff.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-source /usr/lib/openfoam/openfoam2012/etc/bashrc
+source /fsx/OpenFOAM/OpenFOAM-v2212/etc/bashrc
 cd "${0%/*}" || exit                                # Run from this directory
 . ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions        # Tutorial run functions
 #------------------------------------------------------------------------------
 time=$(foamListTimes)
-if [ $time == "" ] || [ $time == "10000" ]; then
+if [[ $time == "" ]] || [[ $time == "10000" ]]; then
     echo "10000"
 else
     pvpython pressureDiff.py $PWD $time
