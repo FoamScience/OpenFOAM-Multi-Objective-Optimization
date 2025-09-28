def52afe2ad1d1d6bef71d14137f53c2b1911a34
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2025-09-28 12:14:23 +0200

    fix: bugs with parameter substitution in openfoam cases

--- src/foambo/common.py
+++ src/foambo/common.py
@@ -207,6 +207,25 @@ def process_input_command(command: str | List[str] | None, case: FoamCase):
     case_name_str = str(case.name)
     return [c.replace("$CASE_PATH", case_path_str).replace("$CASE_NAME", case_name_str) for c in command]
 
+def assign_foam_path(foamfile, dotted_path, new_value):
+    """
+        Given a FoamFile, assign a value to a dotted path 
+    """
+    keys = dotted_path.split(".")
+    for k in keys[:-1]:
+        try:
+            k = int(k)
+        except ValueError:
+            pass
+        foamfile = foamfile[k]
+    last = keys[-1]
+    try:
+        last = int(last)
+    except ValueError:
+        pass
+    foamfile[last] = new_value
+    return foamfile[last]
+
 
 def preprocess_case(parameters, cfg):
     """
@@ -221,13 +240,13 @@ def preprocess_case(parameters, cfg):
 
     # Clone template case
     def prepare_case(
-            path: str,
-            mode: Literal["local", "slurm"],
-            trial_destination: str,
-            runner: str | None = None,
-            log_runner: bool | None = False,
-            remote_status_query: str | None = None,
-            remote_early_stop: str | None = None,
+        path: str,
+        mode: Literal["local", "slurm"],
+        trial_destination: str,
+        runner: str | None = None,
+        log_runner: bool | None = False,
+        remote_status_query: str | None = None,
+        remote_early_stop: str | None = None,
     ):
         templateCase = FoamCase(path)
         newcase = os.path.join(trial_destination, f"{EXPERIMENT_NAME}_trial_"+hash)
@@ -252,25 +271,16 @@ def preprocess_case(parameters, cfg):
             param_file_path = os.path.join(case.path, elm.lstrip('/'))
             with FoamFile(param_file_path) as paramFile:
                 for param in elmv:
-                    splits = elmv[param].split('.')
-                    lvl = paramFile
-                    if len(splits) > 1:
-                        for i in range(1,len(splits)-1):
-                            scp = splits[i]
-                            try:
-                                scp = int(splits[i])
-                            except:
-                                pass
-                            lvl = lvl[scp]
-                        try:
-                            lvl[splits[-1]] = parameters[param]
-                        except:
-                            log.warning(f"Couldn't substitute {param} value in its scope, if it's a dependent parameter, you can ignore this warning")
-                    else:
-                        try:
-                            paramFile[elmv[param]] = parameters[param]
-                        except:
-                            log.warning(f"Couldn't substitute {param} value in its scope, if it's a dependent parameter, you can ignore this warning")
+                    try:
+                        log.debug(f"##### replacing {elmv[param]} with {parameters[param]} in {param_file_path}")
+                        new_val = assign_foam_path(paramFile, elmv[param], parameters[param])
+                        log.debug(f"##### new value = {new_val}")
+                    except Exception:
+                        log.exception(
+                            f"Something went wrong when substituting:\n"
+                            f"{elmv[param]} in {param_file_path} with {parameters[param]}\n"
+                            f"If it's not a dependent parameter, this is serious..."
+                        )
     data["case"] = case
     data["casename"] = case.path
     return data
